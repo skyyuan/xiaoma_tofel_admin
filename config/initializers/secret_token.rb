7be3f35a46b4f69bbf441ddc507416e15fbbfdc7
yuanxiaofei <yuanxiaofei@xiaoma.com>
2014-10-24 18:21:14 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+XiaomaTofelAdmin::Application.config.secret_key_base = '7eca8aec9ac785670b7a87b8d1d1e785681ac1e0f17ca736f419779b2ec0a6eae9fb149f19454718e45d7c4b114aa0410d8166d83930201f60889ca1f76056ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+XiaomaTofelAdmin::Application.config.session_store :cookie_store, key: '_xiaoma_tofel_admin_session'
