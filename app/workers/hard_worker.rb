8eb60b8420d10eeb524df35298bd75cf9c6d96ba
yuanxiaofei <yuanxiaofei@xiaoma.com>
2014-11-13 10:22:24 +0800

    xls文件格式修改 词汇

--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -9,7 +9,7 @@ class HardWorker
     book = Spreadsheet.open "#{Rails.root}/public/system/xls/#{name}"
     sheet1 = book.worksheet 0
     sheet1.each do |row|
-      next if row[0] == 'TPO编号' || row[1].blank?
+      next if row[0] == '分组' || row[1].blank?
       rand_num = rand(1..4)
       b = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
         xml.assessmentItem 'xmlns'=>"http://www.imsglobal.org/xsd/imsqti_v2p1", 'xmlns:xsi'=>"http://www.w3.org/2001/XMLSchema-instance",
@@ -32,22 +32,22 @@ class HardWorker
             end
           end
           xml.itemBody do
-            xml.explanation "#{row[4]}#{row[3]}"
-            xml.symbols "#{row[5]}"
+            xml.explanation "#{row[3]}#{row[2]}"
+            xml.symbols "#{row[4]}"
             xml.audio do
-              xml.url "#{row[6]}"
+              xml.url "#{row[5]}"
             end
             xml.choiceInteraction 'responseIdentifier'=>"RESPONSE", 'shuffle'=>"false", 'maxChoices'=>"1" do
-              xml.prompt "#{row[2]}"
+              xml.prompt "#{row[1]}"
               ['A','B','C','D'].each_with_index do |i,index|
                 if (index+1) == rand_num
-                  xml.simpleChoice "#{row[3]}", 'identifier'=>"#{i}"
+                  xml.simpleChoice "#{row[2]}", 'identifier'=>"#{i}"
                 else
                   count = rand(sheet1.count)
                   if count == 0
                     count = count + 1
                   end
-                  xml.simpleChoice "#{sheet1.row(count)[3]}", 'identifier'=>"#{i}"
+                  xml.simpleChoice "#{sheet1.row(count)[2]}", 'identifier'=>"#{i}"
                 end
               end
             end
@@ -55,23 +55,23 @@ class HardWorker
           xml.responseProcessing 'template'=>"http://www.imsglobal.org/question/qti_v2p1/rptemplates/match_correct"
         end
       end
-      if File.exist?("#{Rails.root}/public/system/xml/word/#{row[2]}.xml") == true
-        system("rm public/system/xml/word/#{row[2]}.xml")
+      if File.exist?("#{Rails.root}/public/system/xml/word/#{row[1]}.xml") == true
+        system("rm public/system/xml/word/#{row[1]}.xml")
       end
-      xml_file = File.new("#{Rails.root}/public/system/xml/word/#{row[2]}.xml", 'wb') #{ |f| f.write(b.to_xml) }
+      xml_file = File.new("#{Rails.root}/public/system/xml/word/#{row[1]}.xml", 'wb') #{ |f| f.write(b.to_xml) }
       xml_file.puts b.to_xml #写入数据
       xml_file.close #关闭文件流
-      @voc = VocabularyQuestion.find_by_word(row[2])
+      @voc = VocabularyQuestion.find_by_word(row[1])
       if @voc.present?
         @voc.destroy
       end
-      group = VocabularyGroup.find_by_sequence_number(row[1])
+      group = VocabularyGroup.find_by_sequence_number(row[0])
       count = VocabularyQuestion.where(vocabulary_group_id: group.id).count
       vocabulary = VocabularyQuestion.new
       vocabulary.sequence_number = count + 1
       vocabulary.content = b.to_xml
       vocabulary.vocabulary_group_id = group.id
-      vocabulary.word = row[2]
+      vocabulary.word = row[1]
       vocabulary.save
     end
     system("rm public/system/xls/#{name}")
