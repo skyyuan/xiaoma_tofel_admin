c10e2da6c7fe9968ec0a729865e072552b31483b
yuanxiaofei <yuanxiaofei@xiaoma.com>
2014-11-20 16:03:04 +0800

    语法 查看 和 编辑功能，登陆页面

--- app/controllers/grammar_questions_controller.rb
+++ app/controllers/grammar_questions_controller.rb
@@ -21,35 +21,42 @@ class GrammarQuestionsController < ApplicationController
   end
 
   def create
-    b = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
-      xml.assessmentItem 'xmlns'=>"http://www.imsglobal.org/xsd/imsqti_v2p1", 'xmlns:xsi'=>"http://www.w3.org/2001/XMLSchema-instance",
-      'xsi:schemaLocation'=>"http://www.imsglobal.org/xsd/imsqti_v2p1  http://www.imsglobal.org/xsd/qti/qtiv2p1/imsqti_v2p1.xsd", 'identifier'=>"textEntry",
-      'title'=>"#{params[:grammar_name]}", 'adaptive'=>"false", 'timeDependent'=>"false" do
-        xml.responseDeclaration 'identifier'=>"RESPONSE", 'cardinality'=>"single", 'baseType'=>"string" do
-          xml.correctResponse do
-            xml.value "#{params[:composition]}"
-          end
-        end
-        xml.itemBody do
-          xml.p "#{params[:title]}"
-          xml.textEntryInteraction 'responseIdentifier'=>'RESPONSE', 'expectedLength'=>'15'
-        end
-        xml.responseProcessing 'template'=>"http://www.imsglobal.org/question/qti_v2p1/rptemplates/map_response"
-      end
-    end
+    b = GrammarQuestion.content_fromat_xml(params)
     count = GrammarQuestion.where(grammar_group_id: params[:unit_id]).count
     grammar = GrammarQuestion.new
     grammar.sequence_number = count + 1
-    grammar.content = b.to_xml
+    grammar.content = b
     grammar.grammar_group_id = params[:unit_id]
     if grammar.save
       xml_file = File.new("#{Rails.root}/public/system/xml/syntax/#{grammar.id}.xml", 'wb')
-      xml_file.puts b.to_xml #写入数据
+      xml_file.puts b #写入数据
       xml_file.close #关闭文件流
       render json: {result: 1}
     else
       render json: {result: 0}
     end
+  end
+
+  def show
+    @grammar_question = GrammarQuestion.find params[:id]
+    @question_content = @grammar_question.parse_xml_to_object
+  end
+
+  def edit
+    @grammar_question = GrammarQuestion.find params[:id]
+    @question_content = @grammar_question.parse_xml_to_object
+  end
+
+  def update
+    xml = GrammarQuestion.content_fromat_xml(params)
+    @grammar_question = GrammarQuestion.find params[:id]
+    @grammar_question.content = xml
+    if @grammar_question.save
+      File.open("#{Rails.root}/public/system/xml/syntax/#{@grammar_question.id}.xml", "wb") do |file|
+        file.write xml
+      end
+    end
+    redirect_to grammar_questions_path
   end
 
   def destroy
--- app/models/vocabulary_question.rb
+++ app/models/vocabulary_question.rb
@@ -24,27 +24,18 @@ class VocabularyQuestion < ActiveRecord::Base
   def self.content_fromat_xml(params)
     b = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
       xml.assessmentItem 'xmlns'=>"http://www.imsglobal.org/xsd/imsqti_v2p1", 'xmlns:xsi'=>"http://www.w3.org/2001/XMLSchema-instance",
-      'xsi:schemaLocation'=>"http://www.imsglobal.org/xsd/imsqti_v2p1  http://www.imsglobal.org/xsd/qti/qtiv2p1/imsqti_v2p1.xsd", 'identifier'=>"choice",
-      'title'=>"Unattended Luggage", 'adaptive'=>"false", 'timeDependent'=>"false" do
-        xml.responseDeclaration 'identifier'=>"RESPONSE", 'cardinality'=>"single", 'baseType'=>"identifier" do
+      'xsi:schemaLocation'=>"http://www.imsglobal.org/xsd/imsqti_v2p1  http://www.imsglobal.org/xsd/qti/qtiv2p1/imsqti_v2p1.xsd", 'identifier'=>"textEntry",
+      'title'=>"#{params[:grammar_name]}", 'adaptive'=>"false", 'timeDependent'=>"false" do
+        xml.responseDeclaration 'identifier'=>"RESPONSE", 'cardinality'=>"single", 'baseType'=>"string" do
           xml.correctResponse do
-            xml.value "#{params[:answer]}"
+            xml.value "#{params[:composition]}"
           end
         end
         xml.itemBody do
-          xml.explanation "#{params[:speech]}#{params[:cn]}"
-          xml.symbols ""
-          xml.audio do
-            xml.url ""
-          end
-          xml.choiceInteraction 'responseIdentifier'=>"RESPONSE", 'shuffle'=>"false", 'maxChoices'=>"1" do
-            xml.prompt "#{params[:word]}"
-            params[:option].each_with_index do |choice,index|
-              xml.simpleChoice "#{choice[1]}", 'identifier'=>"#{choice[0]}"
-            end
-          end
+          xml.p "#{params[:title]}"
+          xml.textEntryInteraction 'responseIdentifier'=>'RESPONSE', 'expectedLength'=>'15'
         end
-        xml.responseProcessing 'template'=>"http://www.imsglobal.org/question/qti_v2p1/rptemplates/match_correct"
+        xml.responseProcessing 'template'=>"http://www.imsglobal.org/question/qti_v2p1/rptemplates/map_response"
       end
     end
 
